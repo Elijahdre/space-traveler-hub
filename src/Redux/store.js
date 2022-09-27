50734fc7fe7f2885d72ef1889cc5332bd1821818
Elijah Odjegba <elijahodjegba@gmail.com>
2022-09-27 14:50:47 +0100

    Add store to the redux file

---
+++ src/Redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rocketReducer from "./rocket/rocketSlice";
+
+const Store = configureStore({
+  reducer: {
+    rocket: rocketReducer
+  }
+});
+
+export default Store;
