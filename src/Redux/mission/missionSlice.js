db953dd5f0d83bada38e047f2990258276707d29
Elijah Odjegba <elijahodjegba@gmail.com>
2022-09-27 15:30:25 +0100

    Implement asyncthunk to fetch data for mission

---
+++ src/Redux/mission/missionSlice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchMissions = createAsyncThunk('mission/fetchMissions', async () => {
+  try {
+    const data = await axios.get("https://api.spacexdata.com/v3/missions");
+    return data.data;
+  } catch (error) {
+    return error.message;
+  }
+})
+
+const MissionSlice = createSlice({
+  name: "missions",
+  initialState: {missions: [], status: "idle"},
+  reducers: {
+    joinMission: (state, { payload }) => {
+
+    }
+  },
+  extraReducers: (Builder) => {
+    Builder
+    .addCase(fetchMissions.pending, (state) => {
+      state.status = "pending";
+    })
+    .addCase(fetchMissions.fulfilled,(state, action) => {
+      const data = [];
+      action.payload.map(x => {
+        let { mission_id, description, mission_name } = x;
+        data.push({ mission_id, description, mission_name });
+        return data;
+      });
+      return {
+        ...state,
+        missions: data,
+        status: "idle",
+      }
+    })
+    .addCase(fetchMissions.rejected, (state, { error }) => {
+      return state.status = error;
+    });
+  },
+});
+
+export const { joinMission } = MissionSlice.actions;
+
+export default MissionSlice.reducer;
