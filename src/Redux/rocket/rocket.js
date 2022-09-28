7c281512fc52efb6dcc49ccbecb5b86c224f098b
Elijah Odjegba <elijahodjegba@gmail.com>
2022-09-28 12:39:28 +0100

    Render display rockets list

--- src/Redux/rocket/rocket.js
+++ src/Redux/rocket/rocket.js
@@ -1,48 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAllRockets } from './rocketSlice';
 
-const Rocket = () => (
-  <div className="flex flex-col w-full gap-2 px-2 md:px-8">
-    <div className="lg:flex gap-2 lg:w-full w-10/12 mx-auto">
-      <div className="bg-blue-400 md:w-8/12 md:mx-auto h-44" />
-      <div className="flex flex-col gap-3 mt-2 lg:mt-0">
-        <span className="font-semibold text-lg lg:text-left text-center">
-          Falcon 2
-        </span>
-        <p className="text-justify">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
-          nisi maiores temporibus molestiae porro. Perferendis natus est non
-          laudantium saepe sapiente quod deleniti, molestiae quis distinctio
-          voluptate dolorum eaque delectus earum voluptatibus ullam ipsam.
-        </p>
-        <button
-          type="button"
-          className="bg-blue-600 text-white font-thin px-3 py-2 w-44 rounded-md mx-auto lg:mx-0"
-        >
-          Reserve Rocket
-        </button>
-      </div>
+const Rocket = () => {
+  const { rockets } = useSelector(selectAllRockets);
+
+  return (
+    <div className="flex flex-col w-full gap-2 px-2 md:px-8">
+      {rockets.map((x) => (
+        <div className="lg:flex gap-2 lg:w-full w-10/12 mx-auto" key={x.id}>
+          <div className="bg-blue-400 md:w-8/12 md:mx-auto h-44">
+            <img src={x.flickr_images[0]} alt={x.rocket_name} className="object-contain" />
+          </div>
+          <div className="flex flex-col gap-3 mt-2 lg:mt-0">
+            <span className="font-semibold text-lg lg:text-left text-center">
+              {x.rocket_name}
+            </span>
+            <p className="text-justify">
+              {x.description}
+            </p>
+            <button
+              type="button"
+              className="bg-blue-600 text-white font-thin px-3 py-2 w-44 rounded-md mx-auto lg:mx-0"
+            >
+              Reserve Rocket
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-    <div className="lg:flex gap-2 lg:w-full w-10/12 mx-auto">
-      <div className="bg-blue-400 md:w-8/12 md:mx-auto h-44" />
-      <div className="flex flex-col gap-3 mt-2 lg:mt-0">
-        <span className="font-semibold text-lg lg:text-left text-center">
-          Falcon 2
-        </span>
-        <p className="text-justify">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
-          nisi maiores temporibus molestiae porro. Perferendis natus est non
-          laudantium saepe sapiente quod deleniti, molestiae quis distinctio
-          voluptate dolorum eaque delectus earum voluptatibus ullam ipsam.
-        </p>
-        <button
-          type="button"
-          className="bg-blue-600 text-white font-thin px-3 py-2 w-44 rounded-md mx-auto lg:mx-0"
-        >
-          Reserve Rocket
-        </button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Rocket;
